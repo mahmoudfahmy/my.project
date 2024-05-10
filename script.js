3b7f3042c73c2f23f371ae2c05b6f3209c70ee75
mahmoudfahmy <mahmoudfahmy55@yahoo.com>
2024-05-10 20:04:37 +0300

    int

--- script.js
+++
@@ -1,7 +0,0 @@
-// JavaScript
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    alert('Username: ' + username + '\nPassword: ' + password);
-});
